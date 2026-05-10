5db470bf2505ebe6668d707916893edc6d092acc
ianrash <ianrash@users.noreply.github.com>
2026-05-10 18:46:21 +0300

    Hardcode Supabase credentials

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = "https://pofpmyiqjtwjesisytbd.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvZnBteWlzanR3amVzaXN0eWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwNjU3NjcsImV4cCI6MjA2MTY1MTc2N30.yVyXjG0J9U8QZ9xX6J5Z1K2N3H8P4F7E6D9C8B7A6X5Y4Z3W2V1U0T9S8R7Q6P5N4M3L2K1J0I";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error('Missing required environment variables:');
